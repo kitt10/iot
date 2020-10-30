9448bf09d977f602729fe6cb7703c81631b12472
kitt10 <bulinmartin@gmail.com>
2020-10-31 00:29:27 +0100

    WebsocketHandler added. JsonHandler added. All fine.

--- egs/voicehome/backend/webserver.py
+++ egs/voicehome/backend/webserver.py
@@ -1,36 +1,74 @@
+from abc import ABC
 from tornado.web import StaticFileHandler, RequestHandler as TornadoRequestHandler, Application as TornadoApplication
 from tornado.websocket import WebSocketHandler
 from tornado.ioloop import IOLoop
 from os.path import dirname, join as join_path
+from json import dumps as dumps_json
 
 
-class VoicehomeMainHandler(TornadoRequestHandler):
+class VoicehomeMainHandler(TornadoRequestHandler, ABC):
 
-    def initialize(self):
-        self.cfg = self.application.settings.get('cfg')
+    @staticmethod
+    def initialize():
+        print('Webserver: New MainHandler.')
 
     def get(self):
-        self.render(join_path(self.cfg.tornado.frontend_relative, self.cfg.tornado.index_page))
+        self.render(join_path(self.application.cfg.tornado.frontend_relative, self.application.cfg.tornado.index_page))
 
-    def data_received(self):
-        pass
+
+class VoicehomeWebSocketHandler(WebSocketHandler, ABC):
+
+    def initialize(self):
+        self.application.ws_clients.append(self)
+        print('Webserver: New WS Client. Connected clients:', len(self.application.ws_clients))
+
+    def open(self):
+        print('Webserver: Websocket opened.')
+        self.write_message('Server ready.')
+
+    def on_message(self, message):
+        print('Webserver: Received WS message:'+str(message))
+
+    def on_close(self):
+        self.application.ws_clients.remove(self)
+        print('Webserver: Websocket client closed. Connected clients:', len(self.application.ws_clients))
+
+
+class VoicehomeJsonHandler(TornadoRequestHandler, ABC):
+
+    @staticmethod
+    def initialize():
+        print('Webserver: New JsonHandler')
+
+    def get(self):
+        self.write(dumps_json(self.application.packet))
 
 
 class VoicehomeTornadoApp(TornadoApplication):
 
-    def __init__(self, cfg):
-        self.cfg = cfg
+    def __init__(self, webserver):
+        self.webserver = webserver
+        self.cfg = webserver.cfg
+
+        self.packet = {}
+        self.ws_clients = []
+
         self.tornado_handlers = [
             (r'/', VoicehomeMainHandler),
+            (r'/websocket', VoicehomeWebSocketHandler),
+            (r'/packet', VoicehomeJsonHandler),
             (r'/(.*)', StaticFileHandler, {
                 'path': join_path(dirname(__file__), self.cfg.tornado.frontend_relative)})
         ]
         self.tornado_settings = {
             "debug": self.cfg.tornado.debug,
-            "autoreload": self.cfg.tornado.debug,
-            "cfg": self.cfg
+            "autoreload": self.cfg.tornado.debug
         }
         TornadoApplication.__init__(self, self.tornado_handlers, **self.tornado_settings)
+
+    def ws_message(self, message):
+        for client in self.ws_clients:
+            self.webserver.iol.spawn_callback(client.write_message, dumps_json(message))
 
 
 class VoicehomeWebserver:
@@ -39,7 +77,7 @@ class VoicehomeWebserver:
         self.engine = engine
         self.cfg = engine.cfg
 
-        self.app = VoicehomeTornadoApp(self.cfg)
+        self.app = VoicehomeTornadoApp(webserver=self)
         self.app.listen(self.cfg.tornado.port)
         self.iol = IOLoop.current()
         print('Webserver: Initialized. Listening on', self.cfg.tornado.port)
--- egs/voicehome/frontend/script.js
+++ egs/voicehome/frontend/script.js
@@ -1,5 +1,35 @@
 
 function onBodyLoad() {
     console.log('Web GUI loaded.')
-    global_variable = 1
+    ws = new WebSocket('ws://localhost:8881/websocket')     // ws is a global variable (index.html)
+    ws.onopen = onSocketOpen
+    ws.onmessage = onSocketMessage
+    ws.onclose = onSocketClose
+}
+
+function onSocketOpen() {
+    console.log("WS client: Websocket opened.")
+}
+
+function onSocketMessage(message) {
+    let data
+    try {
+        data = JSON.parse(message.data)
+    } catch(e) {
+        data = message.data
+    }
+    console.log("WS message:", data)
+    sendToServer("Hi from browser. Got your message.")
+}
+
+function onSocketClose() {
+    console.log("WS client: Websocket closed.")
+}
+
+function sendToServer(message) {
+    let payload = {
+        message: message,
+        second_param: [1, 2]
+    }
+    ws.send(JSON.stringify(payload))
 }
