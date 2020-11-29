0e8d5b38b913f346364d3f59d8c0add6e20e2878
jjsanda <jsanda@students.zcu.cz>
2020-11-29 21:51:33 +0100

    added list of modules and toggles

--- egs/voicehome/frontend/js/script.js
+++ egs/voicehome/frontend/js/script.js
@@ -51,59 +51,72 @@ function fillModules() {
 	let modules = loadJsonHandler().modules;
 	let divModules = document.getElementById("modules");
 
+	let moduleList = document.createElement("ul");
+	moduleList.className = "list-group";
+
 	for (let i_module = 0; i_module < modules.length; i_module++) {
-		let moduleDiv = document.createElement("DIV");
-		moduleDiv.className = "i_module";
+		let moduleListItem = document.createElement("li");
+		moduleListItem.className =
+			"list-group-item d-flex justify-content-between align-items-center";
+		moduleList.appendChild(moduleListItem);
 
-		let moduleTitleDiv = document.createElement("DIV");
-		moduleTitleDiv.className = "module_title";
-		moduleTitleDiv.innerHTML = modules[i_module].module_id;
-		moduleDiv.appendChild(moduleTitleDiv);
+		let moduleTitle = document.createElement("h5");
+		moduleTitle.innerHTML = modules[i_module].module_id;
+		moduleListItem.appendChild(moduleTitle);
 
-		let moduleDescriptionDiv = document.createElement("DIV");
-		moduleDescriptionDiv.className = "module_description";
-		moduleDescriptionDiv.innerHTML = modules[i_module].description;
-		moduleDiv.appendChild(moduleDescriptionDiv);
+		let moduleToggle = document.createElement("input");
+		moduleToggle.type = "checkbox";
+		moduleToggle.checked = true;
+		moduleToggle.attribute("data-toggle") = "toggle";
+		moduleToggle.attribute("data-size") = "sm";
+		// <input type="checkbox" checked data-toggle="toggle" data-size="sm" />
 
-		let moduleMovesDiv = document.createElement("DIV");
-		moduleMovesDiv.className = "module_moves";
-		moduleDiv.appendChild(moduleMovesDiv);
+		// for more information of module uncomment following
 
-		divModules.appendChild(moduleDiv);
+		//  let moduleDescriptionDiv = document.createElement("DIV");
+		// moduleDescriptionDiv.className = "module_description";
+		// moduleDescriptionDiv.innerHTML = modules[i_module].description;
+		// moduleList.appendChild(moduleDescriptionDiv);
 
-		for (let i_move = 0; i_move < modules[i_module].moves.length; i_move++) {
-			let moveDiv = document.createElement("DIV");
-			moveDiv.className = "move";
+		// let moduleMovesDiv = document.createElement("DIV");
+		// moduleMovesDiv.className = "module_moves";
+		// moduleList.appendChild(moduleMovesDiv);
 
-			let moveTitleDiv = document.createElement("DIV");
-			moveTitleDiv.className = "move_title";
-			moveTitleDiv.innerHTML = modules[i_module].moves[i_move].method_name;
-			moveDiv.appendChild(moveTitleDiv);
+		// divModules.appendChild(moduleList);
 
-			let moveDescriptionDiv = document.createElement("DIV");
-			moveDescriptionDiv.className = "move_description";
-			moveDescriptionDiv.innerHTML =
-				modules[i_module].moves[i_move].description;
-			moveDiv.appendChild(moveDescriptionDiv);
+		// for (let i_move = 0; i_move < modules[i_module].moves.length; i_move++) {
+		// 	let moveDiv = document.createElement("DIV");
+		// 	moveDiv.className = "move";
 
-			let moveCallsDiv = document.createElement("DIV");
-			moveCallsDiv.className = "move_calls";
-			moveDiv.appendChild(moveCallsDiv);
+		// 	let moveTitleDiv = document.createElement("DIV");
+		// 	moveTitleDiv.className = "move_title";
+		// 	moveTitleDiv.innerHTML = modules[i_module].moves[i_move].method_name;
+		// 	moveDiv.appendChild(moveTitleDiv);
 
-			moduleMovesDiv.appendChild(moveDiv);
+		// 	let moveDescriptionDiv = document.createElement("DIV");
+		// 	moveDescriptionDiv.className = "move_description";
+		// 	moveDescriptionDiv.innerHTML =
+		// 		modules[i_module].moves[i_move].description;
+		// 	moveDiv.appendChild(moveDescriptionDiv);
 
-			for (
-				let i_call = 0;
-				i_call < modules[i_module].moves[i_move].calls.length;
-				i_call++
-			) {
-				let moveCallDiv = document.createElement("DIV");
-				moveCallDiv.className = "move_call";
-				moveCallDiv.innerHTML = modules[i_module].moves[i_move].calls[i_call]
-					.toString()
-					.replace(",", " ");
-				moveCallsDiv.appendChild(moveCallDiv);
-			}
-		}
+		// 	let moveCallsDiv = document.createElement("DIV");
+		// 	moveCallsDiv.className = "move_calls";
+		// 	moveDiv.appendChild(moveCallsDiv);
+
+		// 	moduleMovesDiv.appendChild(moveDiv);
+
+		// 	for (
+		// 		let i_call = 0;
+		// 		i_call < modules[i_module].moves[i_move].calls.length;
+		// 		i_call++
+		// 	) {
+		// 		let moveCallDiv = document.createElement("DIV");
+		// 		moveCallDiv.className = "move_call";
+		// 		moveCallDiv.innerHTML = modules[i_module].moves[i_move].calls[i_call]
+		// 			.toString()
+		// 			.replace(",", " ");
+		// 		moveCallsDiv.appendChild(moveCallDiv);
+		// 	}
+		// }
 	}
 }
