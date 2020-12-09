d97ee8593e574d2c25ad4727ef58ef4b7d9e83ed
jjsanda <jsanda@students.zcu.cz>
2020-12-09 17:03:51 +0100

    try comunication through socket in sensors

--- egs/voicehome/backend/modules/sensors/sensors.py
+++ egs/voicehome/backend/modules/sensors/sensors.py
@@ -23,8 +23,8 @@ class Sensors(VoicehomeModule):
         query = {'key': 'voicehome/sensors/pressure'}
         res = self.search_mongo(self.id, query)
         print('after res')
-        msg['message'] = res
+        # msg['message'] = res
 
-        self.websocket_send(msg)
+        self.websocket_send(res)
         print("message sended")
         pass
