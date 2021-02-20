e74c939d9538233377050335fbc6ab615d4155ef
Josef Šanda <jsanda@students.zcu.cz>
2021-02-20 19:08:42 +0100

    mongo query

--- egs/voicehome/scripts/mongoQuery.py
+++ egs/voicehome/scripts/mongoQuery.py
@@ -4,7 +4,8 @@ myclient = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
 mydb = myclient["voicehome"]
 mycol = mydb["sensors"]
 
-myquery = { "key": "voicehome/sensors/temperature" }
+# myquery = { "key": "voicehome/sensors/temperature" }
+myquery = { "_id": '600b671f4b673e62da5d99a3' }
 
 mydoc = mycol.find(myquery)
 
