9c2c6ea7121cda010735a3019df486ab9602c1e3
Josef Šanda <jsanda@students.zcu.cz>
2021-02-20 19:38:57 +0100

    mongo update

--- egs/voicehome/scripts/mongoUpdate.py
+++ egs/voicehome/scripts/mongoUpdate.py
@@ -9,6 +9,6 @@ mycol = mydb["sensors"]
 myquery = { "_id": ObjectId('600b66a74b673e62da5d999f') }
 newvalues = { "$set": { "payload": '{"location": "room_1", "owner": "jsanda", "status": "ok", "sensor_id": "room_1", "quantity": "degrees", "timestamp": "2021-01-23 00:58:31", "temperature_value": 18.83333}' } }
 
-x = mycol.updateMany(myquery, newvalues)
+x = mycol.update(myquery, newvalues)
 
 print(x.modified_count, "documents updated.")
