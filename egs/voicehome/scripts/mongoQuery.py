3829399b038e89f0a02be49d4ebb2229fe7349f9
Josef Šanda <jsanda@students.zcu.cz>
2021-02-20 19:16:45 +0100

    mongo update

--- egs/voicehome/scripts/mongoQuery.py
+++ egs/voicehome/scripts/mongoQuery.py
@@ -5,7 +5,9 @@ myclient = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
 mydb = myclient["voicehome"]
 mycol = mydb["sensors"]
 
+# find by topic
 # myquery = { "key": "voicehome/sensors/temperature" }
+# find by id
 myquery = { "_id": ObjectId('600b66a74b673e62da5d999f') }
 
 mydoc = mycol.find(myquery)
---
+++ egs/voicehome/scripts/mongoUpdate.py
@@ -0,0 +1,14 @@
+import pymongo
+from bson.objectid import ObjectId
+
+myclient = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
+mydb = myclient["voicehome"]
+mycol = mydb["sensors"]
+
+# myquery = { "address": { "$regex": "^S" } }
+myquery = { "_id": ObjectId('600b66a74b673e62da5d999f') }
+newvalues = { "$set": { "payload": b'{"location": "room_1", "owner": "jsanda", "status": "ok", "sensor_id": "room_1", "quantity": "degrees", "timestamp": "2021-01-23 00:58:31", "temperature_value": 18.83333}' } }
+
+x = mycol.update_many(myquery, newvalues)
+
+print(x.modified_count, "documents updated.")
