c5a7a522543ea6b1c3f6772c5d0d416e9ba18751
Josef Šanda <jsanda@students.zcu.cz>
2021-02-22 21:13:26 +0100

    myMongoUpdate

--- egs/voicehome/scripts/myMongoUpdate.py
+++ egs/voicehome/scripts/myMongoUpdate.py
@@ -19,31 +19,38 @@ with open('mongo_query_output.txt', 'w') as f:
       # print(x)
       f.write("%s\n" % x['_id'])
 
+mydoc1 = mycol.find()
 with open('mongo_update_wrong_status.txt', 'w') as f:
-    for res in mydoc:
+    for res in mydoc1:
         res_dec = res["payload"].decode("utf8")
         res_json = json.loads(res_dec)
 
         if res_json['location'] == 'room_1':
+            print('room_1')
             res_json['sensor_id'] = 'bme280_1'
             # res_json.pop('status')
             if "humidity_value" in res_json:
+                print('humidity')
                 if res_json["humidity_value"] <= 0 or res_json["humidity_value"]>100:
                     res_json['status'] = 'error'
                     f.write("%s\n" % res['_id'])
             if "temperature_value" in res_json:
+                print('temperature')
                 if res_json["temperature_value"] < -40 or res_json["temperature_value"]>85:
                     res_json['status'] = 'error'
                     f.write("%s\n" % res['_id'])
             if "pressure_value" in res_json:
+                print('press')
                 if res_json["pressure_value"] < 300 or res_json["pressure_value"]>1100:
                     res_json['status'] = 'error'
                     f.write("%s\n" % res['_id'])
 
 
         if res_json['location'] == 'room_2':
+            print('room_2')
             res_json['sensor_id'] = 'ds18b20_1'
             if "temperature_value" in res_json:
+                print('temper')
                 if res_json["temperature_value"] < -55 or res_json["temperature_value"] >125:
                     res_json['status'] = 'error'
                     f.write("%s\n" % res['_id'])
@@ -54,5 +61,6 @@ with open('mongo_update_wrong_status.txt', 'w') as f:
             "payload": json.dumps(res_json)
             }
         }
-        x = mycol.update(myquery, newvalues)
+        # x = mycol.update(myquery, newvalues)
         print(res['_id'], " data updated.")
+        print('newvalues ', newvalues)
