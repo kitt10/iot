14b15ec699af431830e1d40083676865eddb6177
Josef Šanda <jsanda@students.zcu.cz>
2021-03-08 17:26:58 +0100

    try for weather webscrape script

--- egs/voicehome/scripts/webscrape_weather.py
+++ egs/voicehome/scripts/webscrape_weather.py
@@ -4,11 +4,17 @@ import copy
 import urllib.request
 import time
 from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--no-sandbox')
+options.add_argument('--headless')
 
 # mac
-driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
+# driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
 # linux
-# driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
+driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver',chrome_options=options)
 
 URL = 'https://www.chmi.cz/predpovedi/predpovedi-pocasi/ceska-republika/kraje/plzensky'
 driver.get(URL)
