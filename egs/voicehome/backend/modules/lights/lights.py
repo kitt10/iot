ac366b6992a1ad07038b46b838eaddce99c133d9
Josef Šanda <jsanda@students.zcu.cz>
2021-03-08 17:08:34 +0100

    added esp lights status for module lights

--- egs/voicehome/backend/modules/lights/lights.py
+++ egs/voicehome/backend/modules/lights/lights.py
@@ -1,12 +1,30 @@
 from modules.voicehome_module import VoicehomeModule
 import json
 
+
 class Lights(VoicehomeModule):
 
     def __init__(self, engine, dir_path):
         VoicehomeModule.__init__(self, engine, dir_path)
         self.lightsList = {}
         self.reloadLightsList()
+        # self.requestStateEachLight()
+
+
+    def requestState(self,ID,type):
+        payload = {
+            'ID': ID,
+            'type': type
+        }
+        self.mqtt_publish(topic='voicehome/lights/state/command', payload=payload)
+        
+    def requestStateEachLight(self):
+        for esp in self.lightsList['ESP_onboard']:
+            self.requestState(esp['ID'],'ESP_onboard')
+
+        for light in self.lightsList['light']:
+            self.requestState(light['ID'],'light')
+
 
     def ESP_turn_on_light_onboard(self, ESP_ID):
         payload = {
@@ -14,6 +32,9 @@ class Lights(VoicehomeModule):
             'type': 'ESP_onboard',
             'set': 1
         }
+        for esp in self.lightsList['ESP_onboard']:
+            if esp['ID'] == ESP_ID:
+                esp['state'] = 1
         self.mqtt_publish(topic='voicehome/lights/command', payload=payload)
 
     def ESP_turn_off_light_onboard(self, ESP_ID):
@@ -22,6 +43,9 @@ class Lights(VoicehomeModule):
             'type': 'ESP_onboard',
             'set': 0
         }
+        for esp in self.lightsList['ESP_onboard']:
+            if esp['ID'] == ESP_ID:
+                esp['state'] = 0
         self.mqtt_publish(topic='voicehome/lights/command', payload=payload)
 
     def ESP_turn_on_light(self, light_ID):
@@ -30,7 +54,12 @@ class Lights(VoicehomeModule):
             'type': 'light',
             'set': 1
         }
+        for light in self.lightsList['light']:
+            if light['ID'] == light_ID:
+                light['state'] = 1
         self.mqtt_publish(topic='voicehome/lights/command', payload=payload)
+
+
 
     def ESP_turn_off_light(self, light_ID):
         payload = {
@@ -38,12 +67,14 @@ class Lights(VoicehomeModule):
             'type': 'light',
             'set': 0
         }
+        for light in self.lightsList['light']:
+            if light['ID'] == light_ID:
+                light['state'] = 0
         self.mqtt_publish(topic='voicehome/lights/command', payload=payload)
 
     def ESP_turn_on_light_1(self):
         print('Module ' + self.id + ": turning light 1 on")
         self.ESP_turn_on_light(1)
-        self.reply('Rozsvicuji světlo 1')
 
     def ESP_turn_off_light_1(self):
         print('Module ' + self.id + ": turning light 1 off")
@@ -70,17 +101,41 @@ class Lights(VoicehomeModule):
         self.ESP_turn_off_light_onboard(3)
 
     def turn_on_ESP_onboard_led(self):
-        self.ESP_turn_on_light_onboard('all')
+        for esp in self.lightsList['ESP_onboard']:
+            self.ESP_turn_on_light_onboard(esp['ID'])
 
     def turn_off_ESP_onboard_led(self):
-        self.ESP_turn_off_light_onboard('all')
+        for esp in self.lightsList['ESP_onboard']:
+            self.ESP_turn_off_light_onboard(esp['ID'])
 
     def on_mqtt_message(self, msg):
         print('Module ' + self.id + ": start sending mqtt")
+        payload = msg.payload.decode('utf8').replace("'", '"')
+        payload = json.loads(payload)
+        if msg.topic == "voicehome/lights/state/receive":
+            if payload['type'] in self.lightsList.keys():
+                for light in self.lightsList[payload['type']]:
+                    if payload['ID'] == light['ID']:
+                        light['state']=payload['state']
+
+            msg={}
+            msg['passport'] = "lights/state"
+            msg['message'] = "lightsList"
+            msg['reply'] = self.lightsList
+            try:
+                self.websocket_send(msg)
+            except:
+                print('Unenable to send websocket at module Lights on_mqtt_message()')
+                pass
         pass
 
     def on_websocket_message(self, msg):
         print('Module ' + self.id + ": start sending websocket")
+        print(msg)
+        if msg['passport'] == "lights/state":
+            if msg['message'] == "lightsList":
+                msg['reply'] = self.lightsList
+                self.websocket_send(msg)
         pass
 
     def reloadLightsList(self):
