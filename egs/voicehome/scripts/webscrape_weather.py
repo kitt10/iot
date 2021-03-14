489322f3a171c642b2a9b43fedd184873f4f4367
Josef Šanda <jsanda@students.zcu.cz>
2021-03-14 11:16:53 +0100

    script to get analysis about www.chmi.cz changes

--- egs/voicehome/scripts/webscrape_weather.py
+++ egs/voicehome/scripts/webscrape_weather.py
@@ -1,31 +1,54 @@
 from selenium import webdriver
-import requests
-import copy
-import urllib.request
-import time
-from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
+import json
+import time
 
 options = Options()
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--no-sandbox')
 options.add_argument('--headless')
 
-# mac
-# driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
-# linux
-driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver',chrome_options=options)
+while True:
+    # mac
+    # driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver',options=options)
+    # linux
+    driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver',chrome_options=options)
 
-URL = 'https://www.chmi.cz/predpovedi/predpovedi-pocasi/ceska-republika/kraje/plzensky'
-driver.get(URL)
+    URL = 'https://www.chmi.cz/predpovedi/predpovedi-pocasi/ceska-republika/kraje/plzensky'
+    # Stránka připravena 08.03.2021 v 16:01 UTC Swing a.s. Předpověď je vydávána 5xdenně v 5, 10:30, 12, 17 a 22 hodin
+    driver.get(URL)
 
-# a = soup.findAll('a')
-driver.implicitly_wait(1)
-results = driver.find_element_by_id('loadedcontent').text.split('\n\n')
+    # data = {}
+    # data['output']=[]
+    # with open("sample_file.json", "w") as file:
+    #     json.dump(data, file)
+    #
+    # def job_function():
+    #     with open("sample_file.json", "r+") as file:
+    #         data = json.load(file)
 
-# results = soup.find_all('div')
-# print(results.prettify())
-# print(results)
-driver.close()
-print(results[4])
+
+
+
+    # results = driver.find_element_by_id('loadedcontent').text.split('\n\n')
+    # driver.get_screenshot_as_file("screenshot.png")
+
+
+
+    t = time.localtime()
+    current_time = time.strftime("%H_%M_%S", t)
+    print(current_time)
+
+    S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
+    driver.set_window_size(S('Width'),S('Height')) # May need manual adjustment
+    driver.find_element_by_tag_name('body').screenshot('screenshot/web_screenshot_'+current_time+'.png')
+
+    driver.close()
+
+    time.sleep(1800)
+
+
+
+
+# print(results[4])
 print("comp")
