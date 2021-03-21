74fd0795a206fdecfe7ecb19b00af44ed2c17c5b
Josef Šanda <jsanda@students.zcu.cz>
2021-03-21 14:14:28 +0100

    fix path to chromedriver

--- egs/voicehome/backend/modules/weather/weather.py
+++ egs/voicehome/backend/modules/weather/weather.py
@@ -24,7 +24,7 @@ class Weather(VoicehomeModule):
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--no-sandbox')
         options.add_argument('--headless')
-        self.driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', options=options)
+        self.driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver',chrome_options=options)
         self.URL = 'https://www.chmi.cz/predpovedi/predpovedi-pocasi/ceska-republika/kraje/plzensky'
 
         self.regex_patterns_forecast_today = [
