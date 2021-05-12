154d69f4dc09ba14850cb786ac7347f6cad9c98f
pernik36 <pernik36@centrum.cz>
2021-05-12 16:50:43 +0200

    added code for ESP8266

---
+++ egs/smartlights/device/esp_light/config.py
@@ -0,0 +1,17 @@
+MQTT = dict(
+    BROKER = "147.228.124.68",
+    PORT = 1883,
+    USER = "vbrenik",
+    PASSWD = "makamnaprj",
+    STATUS_TOPIC = "smartlights/ESP0/status",
+    COMMAND_TOPIC = "smartlights/ESP0/cmd"
+)
+
+WIFI = dict(
+    SSID = "GUEST",
+    PASSWD = "123454321"
+)
+
+LEDS = dict(
+    BILA = 2
+)
