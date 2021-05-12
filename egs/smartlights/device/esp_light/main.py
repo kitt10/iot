4c333d866a53e61da07fa236b75b13305e834fa1
pernik36 <pernik36@centrum.cz>
2021-05-12 20:31:50 +0200

    changed state reporting format

--- egs/smartlights/device/esp_light/main.py
+++ egs/smartlights/device/esp_light/main.py
@@ -25,21 +25,22 @@ def callback(topic, msg):
                 if(msg_d.get("status") == 1):
                     p.on()
                     states[msg_d.get("LED")] = 1
-                    msg_s = dict(leds = [msg_d.get("LED")], states = [1])
+                    msg_s = dict(states = dict(zip([msg_d.get("LED")], [1])))
                 elif(msg_d.get("status") == 0):
                     p.off()
                     states[msg_d.get("LED")] = 0
-                    msg_s = dict(leds = [msg_d.get("LED")], states = [0])
+                    msg_s = dict(states = dict(zip([msg_d.get("LED")], [0])))
                 else:
                     msg_s = {"error": {"code": 1, "description": "Invalid status, use 0 or 1."}}
             else:
                 msg_s = {"error": {"code": 2, "description": "This LED wasn't found."}}
         elif(msg_d.get("cmd") == "get_status"):
-            msg_s = {"leds": list(cfg.LEDS), "states": list(states.values())}
+            msg_s = dict(states = states)
         else:
             msg_s = {"error": {"code": 3, "description": "Unknown command."}}
     except:
         msg_s = {"error": {"code": 4, "description": "Unexpected error, check payload."}}
+        raise
     finally:
         msg = json.dumps(msg_s)
         client.publish(cfg.MQTT["STATUS_TOPIC"], msg)
