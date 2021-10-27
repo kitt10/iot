a393cabf3ce08916fcc293ec45c41c0c00cd2610
Vojtěch Breník <vojta.brenik@outlook.cz>
2021-10-27 09:34:11 +0200

    added time, reconnect

--- egs/smartblinds/device/esp_lux_temp/main.py
+++ egs/smartblinds/device/esp_lux_temp/main.py
@@ -8,18 +8,18 @@ import time
 import ntptime
 import onewire, ds18x20
 import tsl2591
+from connect import connect
+import network
 
 config = open("config.json", "r")
 cfg = json.load(config)
 config.close()
 
-ntptime.settime()
-
 CLIENT_ID = ubinascii.hexlify(machine.unique_id())
 client = MQTTClient(CLIENT_ID, cfg["mqtt"]["broker"], cfg["mqtt"]["port"], cfg["mqtt"]["user"], cfg["mqtt"]["passwd"])
 client.connect()
-
-ds = ds18x20.DS18X20(onewire.OneWire(Pin(4)))
+ow = onewire.OneWire(Pin(cfg["ds_pin"]))
+ds = ds18x20.DS18X20(ow)
 roms = ds.scan()
 
 sta_if = network.WLAN(network.STA_IF)
@@ -30,13 +30,26 @@ def measure_temp():
 
 tsl = tsl2591.Tsl2591()
 
+def get_time():
+    try:
+        ntptime.settime()
+    except:
+        pass
+    (year, month, mday, hour, minute, sec, wd, yd) = time.localtime(3600*cfg["tz"] + time.mktime(time.localtime()))
+    seconds = hour*3600 + minute*60 + sec
+    return (yd, wd, seconds)
+
 def measure_send():
     lux = tsl.measure()
     temp = measure_temp()
 
-    lux_msg = {"value": lux, "quantity": "illuminance"}
-    temp_msg = {"value": temp, "quantity": "temperature"}
-    print(f'Illuminance: {lux} lux\nTemperature: {temp}°C')
+    time_keys = ["year_day", "week_day", "day_seconds"]
+    time_tuple = get_time()
+    time_dict = {time_keys[i]: time_tuple[i] for i in range(len(time_keys))}
+
+    lux_msg = {"time": time_dict, "value": lux, "quantity": "illuminance"}
+    temp_msg = {"time": time_dict, "value": temp, "quantity": "temperature"}
+    print('Illuminance: {} lux\nTemperature: {}°C'.format(lux, temp))
 
     msgs = [lux_msg, temp_msg]
     topics = [cfg["mqtt"]["lux_topic"], cfg["mqtt"]["temp_topic"]]
@@ -44,9 +57,11 @@ def measure_send():
         client.publish(topic, json.dumps(msg))
     print("Successfully published messages.\n")
 
-try:
-    while 1:
+
+while 1:
+    if sta_if.isconnected():
         measure_send()
         time.sleep(cfg["period"])
-finally:
-    client.disconnect()
+    else:
+        connect()
+        client.connect()
