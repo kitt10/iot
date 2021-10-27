4012707d96e4eb00779ebf47c2d73e21dd99aa70
Vojtěch Breník <vojta.brenik@outlook.cz>
2021-10-27 09:23:47 +0200

    added webREPL, moved connect()

--- egs/smartblinds/device/esp_lux_temp/boot.py
+++ egs/smartblinds/device/esp_lux_temp/boot.py
@@ -1,20 +1,5 @@
-import network
-import machine
-import time
-import ujson as json
-
-config = open("config.json", "r")
-cfg = json.load(config)
-config.close()
-
-def connect():
-    sta_if = network.WLAN(network.STA_IF)
-    ap_if = network.WLAN(network.AP_IF)
-    ap_if.active(False)
-    if not sta_if.isconnected():
-        sta_if.active(True)
-        sta_if.connect(cfg["wifi"]['ssid'],cfg["wifi"]['passwd'])
-        while not sta_if.isconnected():
-            time.sleep_ms(500)
+from connect import connect
+import webrepl
 
 connect()
+webrepl.start()
