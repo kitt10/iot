b74ffe10c462fc6ceee18ad64e3a6b6ec4e1bc9a
Vojtěch Breník <vojta.brenik@outlook.cz>
2022-01-27 15:47:07 +0100

    added 'Control' page

--- egs/smartblinds/gui/config.ts
+++ egs/smartblinds/gui/config.ts
@@ -1,14 +1,20 @@
 import { getCurrentTs } from "./fcn/_tools"
 
-//const engineURL = 'http://localhost'              // your local
-const engineURL = 'http://147.228.124.48'           // kitt-kky
+const engineProtocol = 'http'
+const WSProtocol = 'ws'
+
+//const engineHost = 'localhost'              // your local
+const engineHost = '147.228.124.48'           // kitt-kky
+
 const enginePort = '9777'                           // take it from ../engine/cfg_engine.yml
+const engineURL = `${engineProtocol}://${engineHost}:${enginePort}`
 
 const config = {
     task_file: '../engine/task.yaml',
-    ep_data: engineURL+':'+enginePort+'/ep_data/',
-    ep_control: engineURL+':'+enginePort+'/ep_control/',
-    ep_train: engineURL+':'+enginePort+'/ep_train/',
+    ep_data: engineURL+'/ep_data/',
+    ep_control: engineURL+'/ep_control/',
+    ep_train: engineURL+'/ep_train/',
+    ep_ws: `${WSProtocol}://${engineHost}:${enginePort}/ws/`,
 
     defaultTrainBack: 1638313200.0,                  // 1.12.2021
     defaultShowBack: getCurrentTs() - 1209600,       // last 14 days
