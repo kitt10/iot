8ebe819067c4a22843cafbacd3eb68edd9568d3c
kitt <bulinmartin@gmail.com>
2022-01-28 23:53:31 +0100

    default ffnn model integrated, working

--- egs/smartblinds/engine/lib/cl_ffnn.py
+++ egs/smartblinds/engine/lib/cl_ffnn.py
@@ -1,5 +1,6 @@
 from ._classification import Classifier
 from keras.models import load_model
+from collections import OrderedDict
 import numpy as np
 
 class CL_Ffnn(Classifier):
@@ -10,15 +11,25 @@ class CL_Ffnn(Classifier):
 
         # Load trained model
         self.model = load_model(app.cfg['classification']['ffnn_model'])
-        self.app.cl.log('FFNN model loaded.')
+        print('FFNN model loaded.')
         self.model.summary()
 
-    def control(self, features):
-        print('features:', features)
-        print(self.model.predict(np.array(features)))
+        # Prepare env
+        self.taskF = OrderedDict()
+        self.taskT = OrderedDict()
+        self.get_task_info()
 
-        position = np.random.randint(0, 100)    # TODO
-        tilt = np.random.randint(0, 100)        # TODO
+    def control(self, features):
+        
+        X = np.array([self.make_vector(features, self.taskF)])
+        U = self.model.predict(X)
+
+        position = round(float(U[0][0]), 2)
+        tilt = round(float(U[0][1]), 2)
+
+        print('X:', X)
+        print('U:', U)
+        print('suggested position / tilt:', position, tilt)
             
         return position, tilt
     
@@ -26,3 +37,19 @@ class CL_Ffnn(Classifier):
         self.app.cl.log('Training classifier '+self.name)
         pass
 
+    def get_task_info(self):
+        for feature in self.app.task['features']:
+            self.taskF[feature['name']] = (feature['type'], feature['min'], feature['max'])
+
+        for target in self.app.task['targets']:
+            self.taskT[target['name']] = (target['type'], target['min'], target['max'])
+
+    def make_vector(self, data, taskInfo):
+        return [norm(data[name], *info) for name, info in taskInfo.items()]
+
+# should not be in this file...
+def norm(value, a_type, a_min, a_max):
+    if a_type == 'boolean':
+        return int(value)
+    else:
+        return (value-a_min)/(a_max-a_min)
