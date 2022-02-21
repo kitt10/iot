6526492e2b379d62aa303c0f9850f8dd3a3a6a46
Vojtěch Breník <vojta.brenik@outlook.cz>
2022-02-21 16:42:12 +0100

    bugfixes, added some log messages

--- egs/smartblinds/scripts/mongo2h5.py
+++ egs/smartblinds/scripts/mongo2h5.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from time import mktime
 import numpy as np
 import h5py
+import random
 from random import shuffle
 
 
@@ -50,6 +51,27 @@ def load_task(cfg):
 
     return taskF, taskT
 
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
 def norm(value, a_type, a_min, a_max):
     if a_type == 'boolean':
         return int(value)
@@ -70,8 +92,12 @@ def ratios2ranges(length, ratios, batch_size):
     return [[points[i], points[i+1]] for i in range(len(points)-1)]
 
 def split_data(X, Y, cfg, cfgnn, shuffle_data = True):
+    log("Splitting data", cfg['verbose'])
     if shuffle_data:
+        seed = random.random()
+        random.seed(seed)
         shuffle(X)
+        random.seed(seed)
         shuffle(Y)
 
     split_ranges = ratios2ranges(len(X), cfg['data']['split_ratios'], cfgnn['batch_size'])
@@ -80,13 +106,13 @@ def split_data(X, Y, cfg, cfgnn, shuffle_data = True):
     X_split = dict()
     Y_split = dict()
     for suff in split:
-        print(suff)
         X_split[suff] = np.array(X[split[suff][0]:split[suff][1]])
         Y_split[suff] = np.array(Y[split[suff][0]:split[suff][1]])
     
     return X_split, Y_split
 
 def save2h5(X_split, Y_split, cfg, nn):
+    log('Saving data', cfg['verbose'])
     # Save datasets to a .h5 file
     # Datasets are dictionaries with suffixes as keys ('' for train, '_val' for validation and '_test' for testing)
 
@@ -121,8 +147,10 @@ def main(mongo, task, cfg, cfgnn, include_periodical=True, include_events=True):
 
     where_str = '((this.periodical && %s) || (!this.periodical && %s))%s' % (per, evnt, testing_restr)
 
+    log('Querying raw data', cfg['verbose'])
     # Read data from MongoDB
     data = list(mongo.read(query, where_str))
+    log('Data queried', cfg['verbose'])
 
     # Limits
     taskF = task[0]
@@ -146,19 +174,22 @@ def main(mongo, task, cfg, cfgnn, include_periodical=True, include_events=True):
     k_lstm = cfgnn['lstm']['batch_size'] * b_lstm - t
 
     for i, item in enumerate(data):
+        if cfg['verbose']: 
+            printProgressBar(i, k_)
         if i<=k_ffnn:
             X_ffnn.append(make_vector(item['features'], taskF))
             Y_ffnn.append(make_vector(item['targets'], taskT))
 
         if i<=k_lstm:
             X_lstm.append(make_matrix(data[i:i+t], 'features', taskF))
-            Y_lstm.append(make_vector(item['targets'], taskT))
+            Y_lstm.append(make_vector(data[i+t-1]['targets'], taskT))
+    log("", cfg['verbose'])
 
     data_prep = dict()
     data_prep['ffnn'] = (X_ffnn, Y_ffnn)
     data_prep['lstm'] = (X_lstm, Y_lstm)
 
-    for nn in ['ffnn', 'lstm']:
+    for nn in data_prep:
         X, Y = data_prep[nn]
         X_split, Y_split = split_data(X, Y, cfg, cfgnn[nn])
         save2h5(X_split, Y_split, cfg, nn)
