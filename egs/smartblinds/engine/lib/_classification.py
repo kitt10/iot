156371d0a9ddae6406771476045d0c07f154dd8e
Vojtěch Breník <vojta.brenik@outlook.cz>
2022-02-27 11:25:37 +0100

    moved commons to base class

--- egs/smartblinds/engine/lib/_classification.py
+++ egs/smartblinds/engine/lib/_classification.py
@@ -1,9 +1,11 @@
 import abc
 from ._tools import dump_task
+from collections import OrderedDict
+from keras.models import load_model
 
 class Classifier(abc.ABC):
     
-    def __init__(self, app, name, ind):
+    def __init__(self, app, name, trainable, ind):
         self.app = app
         self.name = name
         self.ind = ind
@@ -12,6 +14,18 @@ class Classifier(abc.ABC):
         self.control_time = 0
         self.last_trained = 0
         self.n_samples = 0
+        self.trainable = trainable
+
+        # Load trained model
+        if trainable:
+            self.model = load_model(app.cfg['classification'][name + '_model'])
+            print(name.upper() + ' model loaded.')
+            self.model.summary()
+
+        # Prepare env
+        self.taskF = OrderedDict()
+        self.taskT = OrderedDict()
+        self.get_task_info()
     
     @abc.abstractmethod
     def control(self, features):
@@ -27,3 +41,10 @@ class Classifier(abc.ABC):
         task['classifiers'][self.ind]['trainTime'] = self.train_time
         task['classifiers'][self.ind]['controlTime'] = self.control_time
         dump_task(self.app.cfg, task)
+
+    def get_task_info(self):
+        for feature in self.app.task['features']:
+            self.taskF[feature['name']] = (feature['type'], feature['min'], feature['max'])
+
+        for target in self.app.task['targets']:
+            self.taskT[target['name']] = (target['type'], target['min'], target['max'])
--- egs/smartblinds/engine/lib/cl_ffnn.py
+++ egs/smartblinds/engine/lib/cl_ffnn.py
@@ -1,23 +1,10 @@
 from ._classification import Classifier
-from keras.models import load_model
-from collections import OrderedDict
 import numpy as np
 
 class CL_Ffnn(Classifier):
     
     def __init__(self, app):
-        Classifier.__init__(self, app, 'ffnn', ind=1)
-        self.trainable = True
-
-        # Load trained model
-        self.model = load_model(app.cfg['classification']['ffnn_model'])
-        print('FFNN model loaded.')
-        self.model.summary()
-
-        # Prepare env
-        self.taskF = OrderedDict()
-        self.taskT = OrderedDict()
-        self.get_task_info()
+        Classifier.__init__(self, app, 'ffnn', True, ind=1)
 
     def control(self, features):
         
@@ -36,13 +23,6 @@ class CL_Ffnn(Classifier):
     def train(self, data):
         self.app.cl.log('Training classifier '+self.name)
         pass
-
-    def get_task_info(self):
-        for feature in self.app.task['features']:
-            self.taskF[feature['name']] = (feature['type'], feature['min'], feature['max'])
-
-        for target in self.app.task['targets']:
-            self.taskT[target['name']] = (target['type'], target['min'], target['max'])
 
     def make_vector(self, data, taskInfo):
         return [norm(data[name], *info) for name, info in taskInfo.items()]
