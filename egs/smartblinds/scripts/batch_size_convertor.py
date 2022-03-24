55bf3208600de64350695a22d99d03ec50815bc6
Vojtěch Breník <vojta.brenik@outlook.cz>
2022-03-24 15:22:57 +0100

    fix

--- egs/smartblinds/scripts/batch_size_convertor.py
+++ egs/smartblinds/scripts/batch_size_convertor.py
@@ -2,21 +2,21 @@ import os
 from keras.models import load_model, save_model
 import sys
 
+path = "../models"
 def convert(filename):
-    if(x.startswith("ffnn_best_h")):
+    if(x.startswith("ffnn")):
         for i in range(2):
             model_cfg['layers'][i]['config']['batch_input_shape'] = (1,15)
             new_model = model.__class__.from_config(model_cfg)
             new_model.set_weights(model.get_weights())
-            save_model(new_model, path+"/converted/"+x[:22]+"pr_"+x[22:])
-    if(x.startswith("lstm_best_h")):
+            save_model(new_model, path+"/converted/"+x[:-29]+"pr_"+x[-29:])
+    if(x.startswith("lstm")):
         for i in range(2):
             model_cfg['layers'][i]['config']['batch_input_shape'] = (1,64,15)
             new_model = model.__class__.from_config(model_cfg)
             new_model.set_weights(model.get_weights())
-            save_model(new_model, path+"/converted/"+x[:22]+"pr_"+x[22:])
+            save_model(new_model, path+"/converted/"+x[:-29]+"pr_"+x[-29:])
 
-path = "../models"
 if len(sys.argv) > 1:
     for mdl in sys.argv[1:]:
         convert(mdl)
